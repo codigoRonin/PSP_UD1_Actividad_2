e0b6f403e44357488b5aff7d9bca15617c069618
Michel <codigoronin@gmail.com>
2022-10-28 19:11:10 +0200

    Upload All Exercices

--- src/Ejercicio8.java
+++ src/Ejercicio8.java
@@ -1,3 +1,18 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Ejercicio8 {
-    
+    public static void main(String[] args) {
+        ProcessBuilder pb = new ProcessBuilder("java", "Ejercicio8");
+        pb.directory(new File("./bin"));
+        pb.redirectInput(new File("./resources/Input08.txt"));
+        pb.redirectOutput(new File("./resources/Output08.txt"));
+        try {
+            pb.start();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+    }
 }
